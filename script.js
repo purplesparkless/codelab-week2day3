e5b65891679069e3d0c72ab1da9aa3db5fa24ed1
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-29 00:50:01 +0100

    week2day2 commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+*/
+const markHeight = '1.69';
+const johnHeight = '1.95';
+////////////////////////////
+const markMass = 78;
+const johnMass = 92;
+
+////////////////////
+const markBMI = Math.round(markMass / markHeight **2);
+const johnBMI = Math.round(johnMass / (johnHeight **2));
+
+/////////////////////////////////
+const markHigherBMI =markBMI > johnBMI;
+
+
+
+
